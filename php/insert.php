7e33b5c11c03046562093706ee14c454949db63c
Todd Cope <xander4jesus@yahoo.com>
2016-01-16 16:22:01 -0600

    Use "" around strings so \r\n is interpreted as carriage return and new line. Ensure all output properly ends in \r\n.

--- php/insert.php
+++ php/insert.php
@@ -15,8 +15,8 @@ $db_fields = array('field_1', 'field_2', 'field_3'); // fields we are interested
 /* command-specific data */
 
 /* connect to database */
-$linkID = mysql_connect($db_host, $db_user, $db_pass) or die("Error: Could not connect to host.");
-mysql_select_db($db_database, $linkID) or die("Error: Could not find database.");
+$linkID = mysql_connect($db_host, $db_user, $db_pass) or die("Error: Could not connect to host.\r\n");
+mysql_select_db($db_database, $linkID) or die("Error: Could not find database.\r\n");
 
 /* build query from passed fields */
 $query = "INSERT INTO " . $db_name . " SET dummy = '66'";
@@ -31,15 +31,15 @@ foreach($db_fields as $field)
 /* command-specific query options */
 
 /* run the query */
-$result = mysql_query($query, $linkID) or die("Error: Invalid Entry.");
+$result = mysql_query($query, $linkID) or die("Error: Invalid Entry.\r\n");
 if(mysql_affected_rows() == 0)
-ßß{
+{
 	// this most likely won't happen
-	print 'Error: Invalid Request\n';
+	print "Error: Invalid Request\r\n";
 	exit;
 }
 
 /* success */
-print 'ack:';
+print "ack:\r\n";
 
 ?>
