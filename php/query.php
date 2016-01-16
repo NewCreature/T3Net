e027dfdde9581b97c98e378391b2941a4ab543af
Todd Cope <xander4jesus@yahoo.com>
2016-01-16 16:17:44 -0600

    Fix bug where output of entry was incorrectly formatted if the last field in the entry was empty. Ensure all output is correctly formatted with a trailing '\r\n' combo.

--- php/query.php
+++ php/query.php
@@ -58,8 +58,8 @@ foreach($arguments as $filter)
 }
 
 /* Connect to database. */
-$linkID = mysql_connect($db_host, $db_user, $db_pass) or die("Error: Could not connect to host.");
-mysql_select_db($db_database, $linkID) or die("Error: Could not find database.");
+$linkID = mysql_connect($db_host, $db_user, $db_pass) or die("Error: Could not connect to host.\r\n");
+mysql_select_db($db_database, $linkID) or die("Error: Could not find database.\r\n");
 
 /* Build query. */
 $query = "SELECT * FROM " . $db_name;
@@ -93,7 +93,7 @@ if($limit > 0)
 	$query .= " LIMIT 0 , " . $limit;
 }
 
-$resultID = mysql_query($query, $linkID) or die("Error: Data not found.");
+$resultID = mysql_query($query, $linkID) or die("Error: Data not found.\r\n");
 
 $output = $output_header . "\r\n\r\n";
 
@@ -105,8 +105,9 @@ for($x = 0; $x < mysql_num_rows($resultID); $x++)
 		$output .= "\t" . $e . ": ";
 		if(strlen($row[$e]) > 0)
 		{
-			$output .= xml_escape($row[$e]) . "\r\n";
+			$output .= xml_escape($row[$e]);
 		}
+		$output .= "\r\n";
 	}
 	$output .= "\r\n";
 }
